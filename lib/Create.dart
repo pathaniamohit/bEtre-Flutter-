e17188aa4ed48dacb528945bfbf3d645a7dfde41
pathaniamohit <pathaniamohit0711raj@gmail.com>
2024-10-05 11:40:09 -0400

    Create.dart:  Select Location

--- lib/Create.dart
+++ lib/Create.dart
@@ -57,6 +57,79 @@ class _CreateScreenState extends State<CreateScreen> {
     }
   }
 
+  Future<void> _showLocationDialog() async {
+    TextEditingController _locationController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // Title
+                const Text(
+                  'Enter Location',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'RobotoSerif',
+                  ),
+                ),
+                const SizedBox(height: 16),
+
+                TextField(
+                  controller: _locationController,
+                  decoration: InputDecoration(
+                    hintText: 'Location',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                  ),
+                ),
+                const SizedBox(height: 24),
+
+                SizedBox(
+                  width: double.infinity,  // Full width button
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _selectedLocation = _locationController.text;
+                      });
+                      Navigator.of(context).pop();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                    ),
+                    child: const Text(
+                      'Continue',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'RobotoSerif',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   Future<void> _createPost() async {
     String content = _postContentController.text.trim();
 
