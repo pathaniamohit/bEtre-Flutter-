577fb0647646a91abaa709420370cd7349aa0615
singhamritpal1919 <130592524+singhamritpal1919@users.noreply.github.com>
2024-10-28 22:26:07 -0400

    Inbox updated

--- lib/Inbox.dart
+++ lib/Inbox.dart
@@ -13,118 +13,65 @@ class _InboxScreenState extends State<InboxScreen> {
   final DatabaseReference _dbRef = FirebaseDatabase.instance.ref();
   User? _user;
 
-  List<Widget> _activityWidgets = [];
+  List<Map<String, dynamic>> _notifications = [];
 
   @override
   void initState() {
     super.initState();
     _user = _auth.currentUser;
     if (_user != null) {
-      _loadRecentActivity();
+      _loadNotifications();
     }
   }
 
-  void _loadRecentActivity() {
-    List<Widget> activityWidgets = [];
-
-    _dbRef.child('posts').orderByChild('userId').equalTo(_user!.uid).onValue.listen((event) {
-      activityWidgets.clear();
+  void _loadNotifications() {
+    _dbRef.child('inbox').child(_user!.uid).orderByChild('timestamp').onValue.listen((event) {
+      List<Map<String, dynamic>> notifications = [];
       if (event.snapshot.exists) {
-        for (var post in event.snapshot.children) {
-          var postData = Map<String, dynamic>.from(post.value as Map);
-          postData['postId'] = post.key;
-
-          // Fetch comments from other users
-          if (postData['comments'] != null) {
-            postData['comments'].forEach((commentId, commentData) {
-              if (commentData['userId'] != _user!.uid) {
-                activityWidgets.add(_buildActivityItem(commentData['userId'], 'commented on your post', commentData['content'], postData));
-              }
-            });
-          }
-
-          // Fetch likes from other users
-          if (postData['likes'] != null) {
-            postData['likes'].forEach((userId, _) {
-              if (userId != _user!.uid) {
-                activityWidgets.add(_buildActivityItem(userId, 'liked your post', null, postData));
-              }
-            });
-          }
+        for (var notificationSnapshot in event.snapshot.children) {
+          Map<String, dynamic> notificationData = Map<String, dynamic>.from(notificationSnapshot.value as Map);
+          notificationData['notificationId'] = notificationSnapshot.key;
+          notifications.add(notificationData);
         }
+        // Sort notifications by timestamp descending
+        notifications.sort((a, b) => b['timestamp'].compareTo(a['timestamp']));
 
         setState(() {
-          _activityWidgets = activityWidgets;
+          _notifications = notifications;
+        });
+      } else {
+        setState(() {
+          _notifications = [];
         });
       }
     });
   }
 
-  Map<String, Map<String, String?>> _userInfoCache = {};
-
-  Future<Map<String, String?>> _getUserInfo(String userId) async {
-    if (_userInfoCache.containsKey(userId)) {
-      return _userInfoCache[userId]!;
+  Future<Map<String, dynamic>> _getUserInfo(String userId) async {
+    DataSnapshot snapshot = await _dbRef.child('users').child(userId).get();
+    if (snapshot.exists) {
+      Map<String, dynamic> userData = Map<String, dynamic>.from(snapshot.value as Map);
+      return userData;
     } else {
-      DataSnapshot snapshot = await _dbRef.child('users').child(userId).get();
-      if (snapshot.exists) {
-        Map<String, dynamic> userData = Map<String, dynamic>.from(snapshot.value as Map);
-        String? username = userData['username'];
-        String? profileImageUrl = userData['profileImageUrl'];
-
-        Map<String, String?> userInfo = {
-          'username': username,
-          'profileImageUrl': profileImageUrl,
-        };
-        _userInfoCache[userId] = userInfo;
-        return userInfo;
-      } else {
-        return {
-          'username': 'Unknown User',
-          'profileImageUrl': null,
-        };
-      }
+      return {'username': 'Unknown User', 'profileImageUrl': null};
     }
   }
 
-  Widget _buildActivityItem(String userId, String action, String? content, Map<String, dynamic> postData) {
-    return FutureBuilder(
-      future: _getUserInfo(userId),
-      builder: (context, AsyncSnapshot<Map<String, String?>> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return ListTile(
-            leading: CircleAvatar(child: CircularProgressIndicator()),
-            title: Text('Loading...'),
-          );
-        } else if (snapshot.hasError || !snapshot.hasData) {
-          return ListTile(
-            leading: CircleAvatar(child: Icon(Icons.error)),
-            title: Text('Error loading user'),
-          );
-        } else {
-          String username = snapshot.data!['username'] ?? 'Unknown User';
-          String? profileImageUrl = snapshot.data!['profileImageUrl'];
-
-          return ListTile(
-            leading: CircleAvatar(
-              backgroundImage: profileImageUrl != null
-                  ? NetworkImage(profileImageUrl)
-                  : AssetImage('assets/profile_placeholder.png') as ImageProvider,
-            ),
-            title: Text('$username $action'),
-            subtitle: content != null ? Text(content) : null,
-            onTap: () => _viewPostDetails(postData),
-          );
-        }
-      },
-    );
-  }
-
-  void _viewPostDetails(Map<String, dynamic> post) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => PostDetailScreen(post: post, currentUser: _user)),
-    );
+  void _viewPostDetails(String postId) async {
+    // Fetch the post data
+    DataSnapshot postSnapshot = await _dbRef.child('posts').child(postId).get();
+    if (postSnapshot.exists) {
+      Map<String, dynamic> postData = Map<String, dynamic>.from(postSnapshot.value as Map);
+      postData['postId'] = postId;
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => PostDetailScreen(post: postData, currentUser: _user)),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Post not found')),
+      );
+    }
   }
 
   @override
@@ -133,9 +80,49 @@ class _InboxScreenState extends State<InboxScreen> {
       appBar: AppBar(
         title: Text('Inbox'),
       ),
-      body: _activityWidgets.isNotEmpty
-          ? ListView(children: _activityWidgets)
+      body: _notifications.isNotEmpty
+          ? ListView.builder(
+        itemCount: _notifications.length,
+        itemBuilder: (context, index) {
+          var notification = _notifications[index];
+          return FutureBuilder(
+            future: _getUserInfo(notification['fromUserId']),
+            builder: (context, AsyncSnapshot<Map<String, dynamic>> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return ListTile(
+                  leading: CircleAvatar(child: CircularProgressIndicator()),
+                  title: Text('Loading...'),
+                );
+              } else if (snapshot.hasError || !snapshot.hasData) {
+                return ListTile(
+                  leading: CircleAvatar(child: Icon(Icons.error)),
+                  title: Text('Error loading user'),
+                );
+              } else {
+                String username = snapshot.data!['username'] ?? 'Unknown User';
+                String? profileImageUrl = snapshot.data!['profileImageUrl'];
+                String action = notification['action'];
+                String content = '';
+                if (notification['commentContent'] != null) {
+                  content = notification['commentContent'];
+                }
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: profileImageUrl != null
+                        ? NetworkImage(profileImageUrl)
+                        : AssetImage('assets/profile_placeholder.png') as ImageProvider,
+                  ),
+                  title: Text('$username $action'),
+                  subtitle: content.isNotEmpty ? Text(content) : null,
+                  onTap: () => _viewPostDetails(notification['postId']),
+                );
+              }
+            },
+          );
+        },
+      )
           : Center(child: Text('No recent activity')),
     );
   }
 }
+
